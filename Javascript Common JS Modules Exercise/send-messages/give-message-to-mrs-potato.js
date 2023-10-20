a70cf7217f588f419698ca46eb1e78f168c48755
Khobie Maseko <88542655+KhobieMaseko@users.noreply.github.com>
2023-10-20 15:38:27 +0200

    Add files via upload

---
+++ Javascript Common JS Modules Exercise/send-messages/give-message-to-mrs-potato.js
@@ -0,0 +1,10 @@
+const sayHelloTo = require('./say-hello-to.js');
+
+function giveMessageToMrsPotato(message) {
+  sayHelloTo("Mrs. Potato");
+  console.log(`(Psst... ${message})`)
+}
+
+giveMessageToMrsPotato("Hi Buzz");
+
+module.exports = giveMessageToMrsPotato;
